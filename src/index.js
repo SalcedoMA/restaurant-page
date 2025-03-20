42f14b7b684070d90e5da2da14966b8da5eb3278
salcedoma <salcedoma.github@gmail.com>
2025-03-20 20:46:40 -0300

    Create object to get the required buttons from the DOM
    >Add event listener to #menu button, to display menu section.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import "./styles.css";
 import { loadHome } from "./homepage";
+import { loadMenu } from "./menu";
 
- const button = document.querySelector('#home');
-button.addEventListener('click', event => {
+ const buttons = {
+    home: document.querySelector("#home"),
+    menu: document.querySelector("#menu"),
+    about: document.querySelector("#about"),
+    contact: document.querySelector("#contact"),
+ }
+
+
+buttons.home.addEventListener('click', event => {
     loadHome();
+});
+
+buttons.menu.addEventListener('click', event => {
+    loadMenu();
 });
