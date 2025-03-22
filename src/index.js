9ad7d7c1dc6da242a41c559e2a46e81adcef425c
salcedoma <salcedoma.github@gmail.com>
2025-03-22 03:21:58 -0300

    Create loadInfo(), render() and transition().
    >loadInfo() now handles all the individual tasks of loadHome, loadMenu, loadAbout and loadContact, given that most used the same base logic.
    >Parameters for loadInfo are passed depending of which os the nav buttons is clicked.
    >loadInfo is divided into three functions: loadInfo, render and transition.
        -render() displays the contents of the tables information of each module.
        -transition() activates the transition animation.
        -loadInfo() calls each function, depending of the conditions.
    
    >import container, tableHTML, wrapper, scrollDown from constants.js.
    >import content tables from each module.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./styles.css";
-import { loadHome } from "./homepage";
-import { loadMenu } from "./menu";
-import { loadAbout } from "./about";
-import { loadContact } from "./contact";
+import { container, tableHTML, wrapper, scrollDown } from "./constants.js";
+import { homeTables } from "./homepage";
+import { menuTables } from "./menu";
+import { aboutTables } from "./about";
+import { contactTables, suggestionBox } from "./contact";
 
  const buttons = {
     home: document.querySelector("#home"),
@@ -11,18 +12,77 @@ import { loadContact } from "./contact";
     contact: document.querySelector("#contact"),
  }
 
+ function transition() {
+    wrapper.classList.toggle("is-open");
+ }
+
+ function render(section, tables) {
+    let alternator = false;
+    container.innerHTML = ``;
+    for (let table of tables) {
+        let image = "";
+        if (table["image"] !== "") {
+             image = document.createElement("img");
+             image.src = table["image"];
+             image.classList.add("info-image");
+        }
+        const newTable = document.createElement("div");
+        newTable.classList.add("info-section");
+        container.append(newTable);
+
+        if (alternator) {
+            newTable.classList.add("reverse");
+            newTable.append(image);
+            newTable.innerHTML += tableHTML;
+        } else {
+            newTable.innerHTML += tableHTML;
+            newTable.append(image);
+        }
+
+        const cloth = newTable.querySelector(".cloth");
+        cloth.classList.add("information");
+        const title = document.createElement("h2");
+        title.classList.add(`${section}-title`);
+        const text = document.createElement("p");
+        text.classList.add(`${section}-description`);
+        title.innerHTML = table["title"];
+        text.innerHTML = table["text"];
+        cloth.append(title, text);
+
+        alternator = !alternator;
+    }
+ }
+
+let currentSection;
+
+ function loadInfo(section, tables, timer) {
+    transition();
+    if (wrapper.classList.contains("is-open")) {
+        render(section, tables)
+        setTimeout(scrollDown, timer);
+    } else if (currentSection !== section) {
+        setTimeout(function() {
+            transition();
+            render(section,tables);
+        }, 1000);
+        setTimeout(scrollDown, 1000+timer);
+    }
+    currentSection = section;
+}
+
 buttons.home.addEventListener('click', event => {
-    loadHome();
+    loadInfo("home", homeTables, 250)
 });
 
 buttons.menu.addEventListener('click', event => {
-    loadMenu();
+    loadInfo("menu", menuTables, 250);
 });
 
 buttons.about.addEventListener('click', event => {
-    loadAbout();
+    loadInfo("about", aboutTables, 225);
 });
 
 buttons.contact.addEventListener('click', event => {
-    loadContact();
+    loadInfo("contact", contactTables, 750);
+    setTimeout(suggestionBox, 1000);
 });
